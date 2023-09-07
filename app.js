c136078ed90cf5899ba9268491a1fdda2a14fec9
mads94i5 <mads94i5@stud.kea.dk>
2023-09-07 13:44:57 +0200

    port 443

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.delete("/mountains/:id", (req, res) => {
 
 
 // const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+const PORT = 443;
 app.listen(PORT, (error) => {
     if (error) {
         console.log("Error starting the server: " + error);
